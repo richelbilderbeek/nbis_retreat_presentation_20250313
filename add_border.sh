1c9bf295b8b86c967941b586dd4f6b34b82eb0c9
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-03-12 07:01:21 +0100

    Progress

--- add_border.sh
+++ add_border.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 convert home.png -bordercolor black -border 1x1 home_with_border.png
+convert bianca_main_page.png -bordercolor black -border 1x1 bianca_main_page_with_border.png
+convert rt.png -bordercolor black -border 1x1 rt_with_border.png
+
